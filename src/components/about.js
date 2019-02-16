6852fdf197a3bc135b17b9a8d41c49b4fd554203
Sherstyuk Anton <asherstuk@saber3d.ru>
2019-02-16 15:36:29 +0300

    update text and contacts

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const text = `Hello! I'm Asya. I make our world brighter! I draw with watercolors, markers and pencils. I keen on children's style, animals and nature. You can feel free to contact me with any idea and we will bring it to a juicy result! I am a Master of Architecture, I worked as an architect restorer, painted silk, wood and glass, and even created jewelery! So let's create something new and interesting together!`;
+const text = `Hello! I'm Asya. I make our world brighter! I draw with watercolors, markers and pencils, keen on children's style, animals and nature. You can feel free to contact me with any idea and we will bring it to a juicy result! I am a Master of Architecture, worked as an architect restorer, painted silk, wood and glass, and even created jewelery! So let's create something new and interesting together!`;
+
+const contactLink = (url, name) => (
+	<div>
+		<a href={url}>{name}</a>
+	</div>
+);
+
 
 export default () => (
 	<div className="about">
@@ -9,11 +16,11 @@ export default () => (
 		<span>{text}</span>
 		<p>You can reach me at:</p>
 		<div id="contacts">
-			<a href="https://www.shutterstock.com/g/anastasiyacraft">shutterstock</a>
-			<a href="https://creativemarket.com/fiolika">creativemarket</a>
-			<a href="https://www.instagram.com/fiolika.art">instagram</a>
-			<a href="https://vk.com/happy_present">vk</a>
-			<a href="mailto:mail@fiolika.art">mail</a>
+			{contactLink('https://www.shutterstock.com/g/anastasiyacraft', 'shutterstock')}
+			{contactLink('https://creativemarket.com/fiolika', 'creativemarket')}
+			{contactLink('https://www.instagram.com/fiolika.art', 'instagram')}
+			{contactLink('https://vk.com/happy_present', 'vk')}
+			{contactLink('mailto:mail@fiolika.art', 'mail')}
 		</div>
 	</div>
 );
