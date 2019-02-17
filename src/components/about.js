31a4dbb00b91f67a2fc794863c23f2dba0d10cf2
SAPer <gsaper@gmail.com>
2019-02-17 14:15:01 +0300

    update contacts and about text

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const text = `Hello! I'm Asya. I make our world brighter! I draw with watercolors, markers and pencils, keen on children's style, animals and nature. You can feel free to contact me with any idea and we will bring it to a juicy result! I am a Master of Architecture, worked as an architect restorer, painted silk, wood and glass, and even created jewelery! So let's create something new and interesting together!`;
+const text = `Hello! I'm Asya. I make our world brighter! I draw with watercolors, markers and pencils, keen on children's style, animals and nature. You can feel free to contact me with any idea and we will bring it to a juicy result! I am a Master of Architecture, worked as an architect restorer, painted silk, wood and glass, and even created jewelry! So let's create something new and interesting together!`;
 
-const contactLink = (url, name) => (
+const contactLink = (url, name, desc) => (
 	<div>
 		<a href={url}>{name}</a>
+		{desc ? <span>{desc}</span> : null}
 	</div>
 );
 
@@ -16,11 +17,12 @@ export default () => (
 		<span>{text}</span>
 		<p>You can reach me at:</p>
 		<div id="contacts">
+			{contactLink('https://www.instagram.com/fiolika.art', 'instagram')}
 			{contactLink('https://www.shutterstock.com/g/anastasiyacraft', 'shutterstock')}
 			{contactLink('https://creativemarket.com/fiolika', 'creativemarket')}
-			{contactLink('https://www.instagram.com/fiolika.art', 'instagram')}
+			{contactLink('https://www.etsy.com/shop/Fiolika', 'etsy')}
 			{contactLink('https://vk.com/happy_present', 'vk')}
-			{contactLink('mailto:mail@fiolika.art', 'mail')}
+			{contactLink('mailto:an.provozina@yandex.ru', 'mail', 'an.provozina@yandex.ru')}
 		</div>
 	</div>
 );
