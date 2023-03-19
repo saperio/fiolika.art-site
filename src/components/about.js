52fa3d108b8aae4d1278b5ac7e41d9c23b672257
saper <gsaper@gmail.com>
2023-03-19 19:02:34 +0200

    about page update

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const text = `Hello! I'm Asya. I make our world brighter! I draw with watercolors, markers and pencils, keen on children's style, animals and nature. You can feel free to contact me with any idea and we will bring it to a juicy result! I am a Master of Architecture, worked as an architect restorer, painted silk, wood and glass, and even created jewelry! So let's create something new and interesting together!`;
+const text1 = `Hello! My name is Anastasia. I am an illustrator. I strive to make our world brighter! Therefore, I love the childish and cute style that I implement in watercolor and digitally. I am a Master of Architecture, in the past, I worked as an architect restorer, painted silk, wood, and glass, and even created jewelry!`;
+const text2 = `You can feel free to contact me with any idea and we will bring it to a juicy result! So let's create something new and exciting together!`;
 
 const contactLink = (url, name, desc) => (
 	<div>
@@ -13,8 +14,9 @@ const contactLink = (url, name, desc) => (
 export default () => (
 	<div className="about">
 		<img id="ava" src="me.jpg" />
-		<p>Hi!</p>
-		<span>{text}</span>
+		<span>{text1}</span>
+		<br></br>
+		<span>{text2}</span>
 		<p>You can reach me at:</p>
 		<div id="contacts">
 			{contactLink('https://www.instagram.com/fiolika.art', 'instagram')}
@@ -25,7 +27,7 @@ export default () => (
 			{contactLink('https://www.maryjane.ru/catalog/author/fiolika/', 'maryjane', 'you can buy my merch here')}
 			{contactLink('https://vk.com/fiolika_art', 'vk')}
 			{contactLink('https://www.twine.net/fiolika', 'twine')}
-			{contactLink('mailto:an.provozina@yandex.ru', 'mail', 'an.provozina@yandex.ru')}
+			{contactLink('mailto:a.provozina@gmail.com', 'mail', 'a.provozina@gmail.com')}
 		</div>
 	</div>
 );
